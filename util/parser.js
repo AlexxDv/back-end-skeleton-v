9f242a500c3e4bf2dbc95751104c84bc330fe5d2
Aleksandar Dimov <alexxdimov@gmail.com>
2023-02-14 10:29:57 +0100

    Update parser.js

--- util/parser.js
+++ util/parser.js
@@ -1,6 +1,8 @@
 function parseError(error) {
   if (error.name == "ValidationError") {
-    return Object.values(error.errors).map((v) => v.message);
+    return Object.values(error.errors).map((v) => v.message); // ако искаме само 1вата грешка Object.values(error.errors)[0].message
+
+
   } else if (Array.isArray(error)) {
     return error.map((x) => x.msg);
   } else {
